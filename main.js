57f97c6f0ef5e92805360be98213df1f2a8af0f2
matheusGamer12344321 <matheus.siebra@escola.pr.gov.br>
2025-05-05 08:35:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+
 const contadores  = document.querySelectorAll(".contador");
 
 const tempObjetivo1 = new Date ("20245-10-05t00:00:00");
@@ -9,38 +10,40 @@ const tempObjetivo4 = new Date ("2025-11-10T00:00:00")
 
 const tempos = [tempObjetivo1, tempObjetivo2, tempObjetivo3, tempObjetivo4]
 
-contadores[0].textContent = tempObjetivo1 - tempoAtual
+contadores[0].textContent = tempObjetivo1 - tempoAtual;
 
 for (let i=0; i < botoes.length; i++){
 
     botoes[i].onclick = function (){
         for (let j =0; j < botoes.length; j++){
 
-            botoes[j].classList.remove('ativo');
-            textos[j].classList.remove('ativo');
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
-        botoes[i].classList.add('ativo');
-        textos[i].classList.add('ativo');
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo")
     }
     
 }
 
-function calculaTempo(){
+function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
-    let tempoFinal = tempObjetivo1 - tempoAtual;
+    let tempoFinal = tempObjetivo - tempoAtual;
 
-let segundo = Math.floor(tempoFinal/1000);
-let minutos = Math.floor(segundos/60);
-let horas = Math.floor(minutos/60);
-let dias = Math.floor(horas/24);
+let segundo = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
 
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
-
+if(tempoFinal > 0){
 return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundo + " Segundos "
-
-
-
+} else {
+    return "Objetivo concluido"}
+    
 }
+
+
