ec5d9dec443fea577d69539e722a4dc74d3e5161
matheusGamer12344321 <matheus.siebra@escola.pr.gov.br>
2025-05-19 08:42:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ const tempObjetivo2 = new Date ("2025-06-05T00:00:00");
 const tempObjetivo3 = new Date ("2025-07-02T00:00:00");
 const tempObjetivo4 = new Date ("2025-11-10T00:00:00")
 
-const tempos = [tempObjetivo1, tempObjetivo2, tempObjetivo3, tempObjetivo4]
+const tempos = [tempObjetivo0, tempObjetivo1, tempObjetivo2, tempObjetivo3]
 
-contadores[0].textContent = tempObjetivo1 - tempoAtual;
+contadores[0].textContent = tempObjetivo0 - tempoAtual;
 
 for (let i=0; i < botoes.length; i++){
 
@@ -36,14 +36,33 @@ let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos / 60);
 let dias = Math.floor(horas / 24);
 
+
+
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
+
+
 if(tempoFinal > 0){
-return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundo + " Segundos "
+    return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundo + " Segundos "
 } else {
     return "Objetivo concluido"}
     
 }
 
+function atualizaCronometro(){
+        
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+    }
+}
 
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro()
